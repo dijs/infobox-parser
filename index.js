af81ac661bf4663e114c0b41d19ceb49586a7f03
Richard van der Dys <richard.vanderdys@gmail.com>
2017-04-06 13:30:19 +0300

    Added more parsing features

--- index.js
+++ index.js
@@ -1,36 +1,91 @@
-const keyValueGlobalPattern = /\|\s?([-\w\s]+)\s+?=\s+([,#}\{-\|\[\]\w\s.]+)?\n/g;
-const keyValuePattern = /\|\s?([-\w\s]+)\s+?=\s+([,#}\{-\|\[\]\w\s.]+)/;
-const innerValuePattern = /\[\[([\w\s\|,]+)\]\]/;
+const keyValueGlobalPattern = /\|\s?([-\w\s]+)\s+?=\s+([,\$#}\{-\|\[\]\w\s.]+)?\n/g;
+const keyValuePattern = /\|\s?([-\w\s]+)\s+?=\s+([,\$#}\{-\|\[\]\w\s.]+)/;
+const innerValuePattern = /\[\[([#\$\(\)\w\s\|,]+)\]\]/;
 
 const extraPropertyPattern = /\n?\s?\|\s?\w+$/;
 const endingPattern = /\n\}\}$/;
 
+const plainListGlobalPattern = /\{\{plainlist\|([^\}\}]+)\}\}/g;
+const plainListItemPattern = /\*\s?([\(\),#}\{-\|\[\]\w\s.]+)/g;
+
+const commentsPattern = /<!--.*-->/g;
+const listItemPrefixPattern = /^\*\s?/;
+
+const plainListVariablePattern = /\$PLAIN_LIST_(\d)/;
+
+function getValue(raw) {
+  const cleansed = raw
+    .trim()
+    .replace(extraPropertyPattern, '')
+    .replace(endingPattern, '');
+  const result = innerValuePattern.exec(cleansed);
+  if (result) {
+    const innerValue = result[1];
+    const orPosition = innerValue.indexOf('|');
+    if (orPosition !== -1) {
+      return innerValue.substring(0, orPosition);
+    }
+    return innerValue;
+  }
+  if (cleansed === 'y') {
+    return true;
+  }
+  return cleansed;
+}
+
+function findPlainLists(source) {
+  const plainListMatches = source
+    .match(plainListGlobalPattern);
+  if (!plainListMatches) {
+    return {
+      plainLists: [],
+      sourceAfterPlainLists: source,
+    }
+  }
+  const plainLists = plainListMatches.map(match => {
+    const cleanMatch = match.replace(commentsPattern, '');
+    const plainListItemsRaw = cleanMatch.match(plainListItemPattern);
+    const plainListItems = plainListItemsRaw
+      .map(raw => raw.replace(listItemPrefixPattern, '').trim())
+      .map(getValue);
+    return plainListItems;
+  });
+  const sourceAfterPlainLists = plainListMatches.reduce((memo, match, index) => {
+    return memo.replace(match, `$PLAIN_LIST_${index}`);
+  }, source);
+  return {
+    plainLists,
+    sourceAfterPlainLists,
+  };
+}
+
+function reduceVariable(value, { plainLists }) {
+  if (typeof value === 'boolean') {
+    return value;
+  }
+  if (value.match(plainListVariablePattern)) {
+    const [, index] = plainListVariablePattern.exec(value);
+    return plainLists[parseInt(index, 10)];
+  }
+  return value;
+}
+
 module.exports = function (source) {
-  const propertyMatches = source
+  const cleanSource = source
     .replace('&nbsp;', ' ')
-    .replace('|\'\'See list\'\'', '')
+    .replace('|\'\'See list\'\'', '');
+
+  const { sourceAfterPlainLists, plainLists } = findPlainLists(cleanSource);
+
+  // console.log(plainLists);
+  // console.log(sourceAfterPlainLists);
+
+  const propertyMatches = sourceAfterPlainLists
     .match(keyValueGlobalPattern);
 
   // console.log(propertyMatches);
 
-  function getValue(raw) {
-    const cleansed = raw
-      .trim()
-      .replace(extraPropertyPattern, '')
-      .replace(endingPattern, '');
-    const result = innerValuePattern.exec(cleansed);
-    if (result) {
-      const innerValue = result[1];
-      const orPosition = innerValue.indexOf('|');
-      if (orPosition !== -1) {
-        return innerValue.substring(0, orPosition);
-      }
-      return innerValue;
-    }
-    return cleansed;
-  }
-
-  const properties = propertyMatches
+  const propertyList = propertyMatches
     .map(match => {
       const result = keyValuePattern.exec(match);
       if (!result) {
@@ -43,12 +98,11 @@ module.exports = function (source) {
       };
     });
 
-  return properties.reduce((memo, keyValue) => {
+  const properties = propertyList.reduce((memo, { key, value }) => {
     return Object.assign({}, memo, {
-      [keyValue.key]: keyValue.value,
+      [key]: reduceVariable(value, { plainLists }),
     });
   }, {});
-};
 
-// console.log(JSON.stringify(properties, null, 3));
-// console.log(properties.length);
+  return properties;
+};
---
+++ test/batman-spec.js
@@ -0,0 +1,25 @@
+require('should');
+const fs = require('fs');
+const parse = require('../index');
+
+describe('Should Parse Batman\'s Information', () => {
+  const source = fs.readFileSync('./batman.txt', 'utf8');
+  const properties = parse(source);
+  it('publisher', () => {
+    properties.should.have.property('publisher', 'DC Comics');
+  });
+  it('hero', () => {
+    properties.hero.should.be.true();
+  });
+  it('partners', () => {
+    properties.partners.length.should.equal(5);
+    properties.partners.should.containEql('Superman');
+  });
+  it('aliases', () => {
+    properties.aliases.should.containEql('Matches Malone');
+  });
+  it('alliances', () => {
+    properties.alliances.should.containEql('Justice League');
+    properties.alliances.should.containEql('Batman Incorporated');
+  });
+});
