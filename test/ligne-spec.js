e6ed903b75e958e2b34079443679606096fef7f8
Richard van der Dys <richard.vanderdys@gmail.com>
2020-05-14 19:38:04 +0300

    Added better support for parsing Bahnstrecke routes

--- test/ligne-spec.js
+++ test/ligne-spec.js
@@ -6,7 +6,17 @@ describe('BS Table', () => {
   const source = fs.readFileSync('./data/ligne.txt', 'utf8');
   const properties = parse(source);
   it('should parse BS table', () => {
-    properties.bsTables[1].should.be.Array();
-    properties.bsTables[1][0].should.startWith('BS');
+    properties.bsTables[0].type.should.equal('bis');
+    properties.bsTables[0].text2.should.equal('Ligne de Bourges à Miécaze');
+
+    properties.bsTables[7].text2.should.equal(
+      'Viaduc de Sainte-Agathe (ruisseau)'
+    );
+    properties.bsTables[7].comment.should.equal('(165m)');
+    properties.bsTables[7].margin.should.equal('332,721');
+
+    properties.bsTables[20].margin.should.equal('372,2xx/378,931');
+    properties.bsTables[20].text1.should.equal('Gare de Gouttières');
+    properties.bsTables[20].text2.should.equal('(658m)');
   });
 });
--- util/parseBsTables.js
+++ util/parseBsTables.js
@@ -1,4 +1,5 @@
 const linkPattern = /\[\[([^\]]+)\]\]/g;
+const kmPattern = /{{BSkm\|([\dx]+,[\dx]+)\|([\dx]+,[\dx]+)}}/g;
 
 function clean(text) {
   return text
@@ -7,10 +8,41 @@ function clean(text) {
     .replace(/'''?/g, '');
 }
 
+// Source: https://en.wikipedia.org/wiki/Wikipedia:Route_diagram_template#{{BS-map}}_rows_with_icons_and_text
+function parseRow(raw) {
+  let countMatch = raw[0].match(/BS(\d)/);
+  let type = raw[0].substring(2);
+  let count = 1;
+  let icons = [];
+  let index = 1;
+  if (countMatch) {
+    count = countMatch[1];
+    type = raw[0].substring(3);
+  }
+  for (; index <= count; index++) {
+    icons.push(raw[index]);
+  }
+  const margin = raw[index++];
+  const text1 = raw[index++];
+  const text2 = raw[index++];
+  const comment = raw[index++];
+  return {
+    type,
+    margin,
+    text1,
+    text2,
+    comment,
+    icons,
+  };
+}
+
 export default function (source) {
-  const results = source.match(/{{BS([^}}]+)}}/g);
+  const results = source.replace(kmPattern, '$1/$2').match(/{{BS([^}}]+)}}/g);
   if (results) {
-    return results.map((result) => clean(result).split('|'));
+    return results
+      .map((result) => clean(result).split('|'))
+      .map(parseRow)
+      .filter(({ type }) => type[0] !== '-');
   }
   return null;
 }
