983e29e0c09a8da0b7108b633041b0b2b7c6a714
Xavier Carol <xcarolr@gmail.com>
2024-01-05 17:58:14 +0100

    add test for issue #40

---
+++ test/bob-marley-spec.js
@@ -0,0 +1,15 @@
+require('should');
+import fs from 'fs';
+import parse from '../index';
+
+describe.only("Should Parse Bob Marley's Information", () => {
+  const source = fs.readFileSync('./data/bob-marley.txt', 'utf8');
+  const properties = parse(source);
+
+  it('should get children', () => {
+    properties.general.should.have.property(
+      'children',
+      []
+    );
+  });
+});
